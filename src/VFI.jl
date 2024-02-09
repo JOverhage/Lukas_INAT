3d57ce4a08d08ddb3abe82ad62e9ff60ea9c5285
JOverhage <jonasoverhage@outlook.de>
2024-02-09 20:42:52 +0100

    first Vupdate iteration, proper expectation formation outstanding

--- src/VFI.jl
+++ src/VFI.jl
@@ -4,10 +4,15 @@
 
 function guess_V(param::model,pzvec::Vector)
     @unpack_model param
-    # guess an na x nz x nk matrix
+    # guess: Consume 10% of Assets, ignore any inattention and future values:
+    Vg = zeros(na,nz,nk)
+    for (ia,iz,ik) in stateind
+        Vg[ia,iz,ik] = u(param,w + (pzvec[iz] + γgrid[iz])*agrid[ia])
+    end
+    return Vg
 end
 
-function V_update(param::model,pz::Vector,Vnext::Matrix)
+function V_Update(param::model,pz::Vector,Vnext)
     @unpack_model param
     # update V using utility function and the transition probabilities
     # setup Vnew, apol:
@@ -19,10 +24,10 @@ function V_update(param::model,pz::Vector,Vnext::Matrix)
     # now loop over all states today, find maximum values:
     for (ia,iz,ik) in stateind # LOOP OVER STATES today
         # for state today, setup a na' x 1 vector of Value function:
-        coh = w + (pz(iz) + γgrid(iz))*agrid[ia]
+        coh = w + (pz[iz] + γgrid[iz])*agrid[ia]
         # compute value at all possible a' choices:
         for iaprime in 1:na
-            Vchoices[iaprime] = u(coh - agrid[iaprime]) + β*EV[iaprime,iz,ik]
+            Vchoices[iaprime] = u(param,coh - agrid[iaprime]) + β*EV[iaprime,iz,ik]
         end
         # find the maximum value and the corresponding a':
         vn_now,ap_now = findmax(Vchoices)
@@ -32,14 +37,14 @@ function V_update(param::model,pz::Vector,Vnext::Matrix)
     return Vnew,apol
 end
 
-function cont_val(param::model,Vnext::Matrix)
+function cont_val(param::model,Vnext)
     @unpack_model param
     # update V using utility function and the transition probabilities
     cval = zeros(na,nz,nk)
     ## loop over all possible states today:
     # Assets tomorrow, expectation over z today (goes to h or l) -- k today (goes to 0 or k+1)
     for (ia,iz,ik) in stateind 
-        cval[ia,iz,ik] = sum( γprob[iz,izprime]*θ*Vnext[ia,izprime,max[nk,ik+1]] + (1-θ)*Vnext[ia,izprime,1] for izprime in 1:nz)
+        cval[ia,iz,ik] = sum( γprob[iz,izprime]*θ*Vnext[ia,izprime,min(nk,ik+1)] + (1-θ)*Vnext[ia,izprime,1] for izprime in 1:nz)
     end
     return cval
 end
